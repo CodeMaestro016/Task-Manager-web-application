3e87d01cc90db46d214271b85e8d24a94b1c4f5a
Avishka Piyumal <avishkapiyumal16@gmail.com>
2025-08-18 17:57:59 +0530

    Design Home Page Step 2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
         {token ? (
           <>
             <Link 
-              to="/" 
+              to="/dashboard" 
               className="text-lg font-medium hover:bg-white hover:bg-opacity-20 px-4 py-2 rounded-lg transition-all duration-300 ease-in-out transform hover:-translate-y-0.5"
             >
               Dashboard
